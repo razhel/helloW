1cfa95fd575f6e3170436b816b0f86b4fc820cba
vagrant <vagrant@localhost.localdomain>
2017-10-30 14:32:53 +0000

    i+10, sh deleted

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
-	public static void main(String[] args) {
-		for(int i = 0; i < 5, i++) {
-			System.out.println("Hello World!");
-		}
+   public static void main(String[] args) {
+
+      // Prints "Hello, World" in the terminal window.
+	for (int i=0; i<10; i++) {
+	      System.out.println("Hello, World ..."+i);
 	}
+   }
+}
+
--- jenkins5676573438118376168.sh
+++
@@ -1,6 +0,0 @@
-
-echo $PWD >> /tmp/hellooW
-sleep 5
-cd /home/vagrant/helloW
-javac HelloWorld.java
-java /home/vagrant/helloW/HelloWorld 
